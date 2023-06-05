91f24dde8a92332a39779a402f44e1f570b989be
Anastasia <anastasia.hrynkevich@outlook.com>
2023-06-05 11:45:55 +0100

    Airbnb copy - started to create Airbnb app - added needed folders

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <nav className="nav-style">
+            <img src="./imgs/logo.svg" alt="Airbnb logo"/>
+        </nav>
+    )
+}
